3917ac7a22b8e1fc215aa348cd5ef8177ac4614b
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2020-02-27 18:22:06 +0100

    1.12

--- cmake/FindSteamworksSDK.cmake
+++ cmake/FindSteamworksSDK.cmake
@@ -27,5 +27,26 @@ if (NOT STEAMWORKSSDK_FOUND)
 		"${STEAMWORKSSDK_INCLUDE_DIR}"
 	)
 
-	find_package_handle_standard_args(STEAMWORKSSDK DEFAULT_MSG STEAMWORKSSDK_LIBRARY STEAMWORKSSDK_INCLUDE_DIR)
+	#-----------------------------------------
+
+  find_library(
+		STEAMWORKSSDK_APPTICKET_LIBRARY
+		sdkencryptedappticket
+		sdkencryptedappticket64
+		PATH_SUFFIXES
+		lib/steam
+	)
+
+	add_library(STEAMWORKSSDK::APPTICKET STATIC IMPORTED)
+
+	set_target_properties(
+		STEAMWORKSSDK::APPTICKET
+		PROPERTIES
+		IMPORTED_LOCATION
+		"${STEAMWORKSSDK_APPTICKET_LIBRARY}"
+	)
+
+	#-----------------------------------------
+
+	find_package_handle_standard_args(STEAMWORKSSDK DEFAULT_MSG STEAMWORKSSDK_LIBRARY STEAMWORKSSDK_APPTICKET_LIBRARY STEAMWORKSSDK_INCLUDE_DIR)
 endif()
